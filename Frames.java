fc91f6b45afb64c62667aa430f857f8f25bf6aa1
dharanitirumalasetti <147476313+dharanitirumalasetti@users.noreply.github.com>
2024-07-31 15:19:46 +0530

    import java.util.Scanner; public class Frames{     public static void main(String[] args){         int a,b,c,d;         Scanner read=new Scanner(System.in);         a=read.nextInt();         b=read.nextInt();         c=read.nextInt();         d=(2*(a+b)*c);         System.out.println(d);     } }

---
+++ Frames.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Frames{
+    public static void main(String[] args){
+        int a,b,c,d;
+        Scanner read=new Scanner(System.in);
+        a=read.nextInt();
+        b=read.nextInt();
+        c=read.nextInt();
+        d=(2*(a+b)*c);
+        System.out.println(d);
+    }
+}
